57dafb3e988c381ce901ec6ead0f11357b3e9e52
Julio C. Rocha <julio.rocha@microsoft.com>
2022-12-16 17:16:44 -0800

    Define getStarCount

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -65,3 +65,26 @@ function logMessage(message) {
 
   return message;
 }
+
+/**
+ * Gets the star count for a given Github repository.
+ * @customfunction
+ * @param {string} userName string name of Github user or organization.
+ * @param {string} repoName string name of the Github repository.
+ * @return {number} number of stars given to a Github repository.
+ */
+async function getStarCount(userName, repoName) {
+  try {
+    //You can change this URL to any web request you want to work with.
+    const url = "https://api.github.com/repos/" + userName + "/" + repoName;
+    const response = await fetch(url);
+    //Expect that status code is in 200-299 range
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const jsonResponse = await response.json();
+    return jsonResponse.watchers_count;
+  } catch (error) {
+    return error;
+  }
+}
